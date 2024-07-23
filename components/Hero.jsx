9b44198c63e54e8392b9f272fbcd7b8317ccef41
Syed Owais Nawaz <owaisnawaz17@gmail.com>
2024-07-23 23:49:19 +0530

    Add about section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Download, Send } from "lucide-react";
+
+import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+
+// components
+import DevImg from "./DevImg";
+import Badge from "./Badge";
+import Socials from "./Socials";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none bg-transparent dark:bg-transparent">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          {/* text */}
+          <div className="flex w-[600px] xl:w-[640px] max-w-[640px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              AI/ML | Web Developer
+            </div>
+            <h1 className="h1 mb-4">Hello, my name is Syed Owais Nawaz</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              I am a 4th year Computer Engineering student at Jamia Millia
+              Islamia. My intrests are AI/ML and Web Development
+            </p>
+            {/* Buttons */}
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  Contact me <Send size={18} />
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2">
+                Download CV <Download size={18} />
+              </Button>
+            </div>
+            {/* Socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
+          </div>
+          {/* image */}
+          <div className="hidden xl:flex relative">
+            {/* Badge */}
+            {/* <Badge icon={<RiBriefcase4Fill />} /> */}
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer7.png"
+            />
+          </div>
+        </div>
+        {/* icon */}
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
---
+++ components/Socials.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  RiLinkedinFill,
+  RiGithubFill,
+  RiTwitterXFill,
+  RiInstagramFill,
+} from "react-icons/ri";
+
+import Link from "next/link";
+
+const icons = [
+  {
+    path: "/",
+    name: <RiLinkedinFill />,
+  },
+  {
+    path: "/",
+    name: <RiGithubFill />,
+  },
+  {
+    path: "/",
+    name: <RiTwitterXFill />,
+  },
+  {
+    path: "/",
+    name: <RiInstagramFill />,
+  },
+];
+
+const Socials = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link href={icon.path} key={index}>
+            <div className={`${iconStyles}`}>{icon.name}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Socials;
